2e72ca2a83acd2d9b82e77ff7a309a8c55c24b53
John <johnflober@gmail.com>
2019-10-26 10:48:42 -0500

    Fix conflicts

--- src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -1,13 +1,8 @@
 package com.appsdeveloperblog.app.ws.ui.controller;
 
-<<<<<<< HEAD
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-=======
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.*;
-=======
->>>>>>> fdbd30200b6a1a4285a89dc2cf4b083f47a25c33
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +10,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
->>>>>>> a9af1c8f0d479821ed0ec76d987b8aff43698bcf
 
 import com.appsdeveloperblog.app.ws.service.UserService;
 import com.appsdeveloperblog.app.ws.shared.dto.UserDto;
@@ -25,28 +19,6 @@ import com.appsdeveloperblog.app.ws.ui.model.response.UserRest;
 @RestController
 @RequestMapping("users") //http://localhost:8080/users
 public class UserController {
-<<<<<<< HEAD
-
-    @GetMapping
-    public String getUser(){
-        return "get user was called";
-    }
-
-    @PostMapping
-    public String createUser(){
-        return "create user was called";
-    }
-
-    @PutMapping
-    public String updateUser(){
-        return "update user was called";
-    }
-
-    @DeleteMapping
-    public  String deleteUser(){
-        return "delete user was called";
-    }
-=======
 	
 	@Autowired
 	UserService userService;
@@ -79,6 +51,5 @@ public class UserController {
 	public String deleteUser() {
 		return "delete user was called";
 	}
->>>>>>> a9af1c8f0d479821ed0ec76d987b8aff43698bcf
 
 }
