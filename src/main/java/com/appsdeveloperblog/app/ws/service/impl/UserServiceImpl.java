1d5e74b5d3db075688503803e756225e91e0d870
John <johnflober@gmail.com>
2019-10-27 23:10:03 -0500

    test

--- src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImpl.java
+++ src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImpl.java
@@ -24,7 +24,7 @@ public class UserServiceImpl implements UserService {
 	public UserDto createUser(UserDto user) {
 		
 		if(userRepository.findByEmail(user.getEmail()) != null) throw new RuntimeException("Record already exists");
-		
+		//
 		UserEntity userEntity = new UserEntity();
 		BeanUtils.copyProperties(user, userEntity);
 		
