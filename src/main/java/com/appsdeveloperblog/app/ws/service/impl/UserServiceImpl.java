424908634db2bb0b80d86c48f785605de688dfb7
John <johnflober@gmail.com>
2019-10-27 21:33:40 -0500

    Add auto generated UserId

--- src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImpl.java
+++ src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import com.appsdeveloperblog.app.ws.UserRepository;
 import com.appsdeveloperblog.app.ws.io.entity.UserEntity;
 import com.appsdeveloperblog.app.ws.service.UserService;
+import com.appsdeveloperblog.app.ws.shared.Utils;
 import com.appsdeveloperblog.app.ws.shared.dto.UserDto;
 
 //Create DB entries
@@ -16,6 +17,9 @@ public class UserServiceImpl implements UserService {
 	@Autowired
 	UserRepository userRepository;
 	
+	@Autowired
+	Utils utils;
+	
 	@Override
 	public UserDto createUser(UserDto user) {
 		
@@ -24,8 +28,9 @@ public class UserServiceImpl implements UserService {
 		UserEntity userEntity = new UserEntity();
 		BeanUtils.copyProperties(user, userEntity);
 		
+		String publicUserId = utils.generateUserId(30);
+		userEntity.setUserId(publicUserId);
 		userEntity.setEncryptedPassword("test");
-		userEntity.setUserId("testUserId");
 		
 		UserEntity storedUserDetails = userRepository.save(userEntity);
 		
